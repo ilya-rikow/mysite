ee7184e2a3bb88a9baf2229c16384e13a169ae46
ilya-rikow <62751670+ilya-rikow@users.noreply.github.com>
2020-08-21 23:39:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active'
+    },
+});
